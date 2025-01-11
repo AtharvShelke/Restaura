9d6d62977506a39fdbe605567b5497dc94ea2663
AtharvShelke <shelkeatharv964@gmail.com>
2025-01-11 21:46:04 +0530

    updated dish csrd for animation

--- src/components/DishCard.jsx
+++ src/components/DishCard.jsx
@@ -1,6 +1,12 @@
+import { motion } from "framer-motion";
 const DishCard = ({ project: { image, title, description } }) => {
   return (
-    <article className="rounded-3xl shadow-md">
+    <motion.article
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.6, delay: 0.4 }}
+      className="rounded-3xl shadow-md">
       <img
         src={image || "/placeholder.jpg"}
         alt={`${title} image`}
@@ -10,7 +16,7 @@ const DishCard = ({ project: { image, title, description } }) => {
         <h3 className="mb-2 text-2xl font-bold tracking-tighter">{title}</h3>
         <p className="text-sm text-neutral-600">{description}</p>
       </div>
-    </article>
+    </motion.article>
   );
 };
 
