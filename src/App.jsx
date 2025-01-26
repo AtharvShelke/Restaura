20b8981ece6e9eaae8a5918012ae1f55d86796fc
AtharvShelke <shelkeatharv964@gmail.com>
2025-01-26 16:51:38 +0530

    added locomotive scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import About from "./components/About";
 import ContactSection from "./components/ContactSection";
 import Dishes from "./components/Dishes";
@@ -7,20 +8,35 @@ import HeroSection from "./components/HeroSection";
 import Mission from "./components/Mission";
 import Navbar from "./components/Navbar";
 import Review from "./components/Review";
-
+import LocomotiveScroll from "locomotive-scroll";
 
 export default function App() {
+  useEffect(() => {
+    // Initialize Locomotive Scroll
+    const locomotiveScroll = new LocomotiveScroll({
+      el: document.querySelector(".scroll-container"), // Target the scroll container
+      smooth: true, // Enable smooth scrolling
+    });
+
+    // Cleanup on component unmount
+    return () => {
+      locomotiveScroll.destroy();
+    };
+  }, []);
+
   return (
     <main className="overflow-y-hidden text-neutral-200 antialiased">
-      <HeroSection />
-      <Navbar />
-      <Dishes/>
-      <About/>
-      <Mission/>
-      <Expertise/>
-      <Review/>
-      <ContactSection/>
-      <Footer/>
+      <div className="scroll-container">
+        <HeroSection />
+        <Navbar />
+        <Dishes />
+        <About />
+        <Mission />
+        <Expertise />
+        <Review />
+        <ContactSection />
+        <Footer />
+      </div>
     </main>
-  )
+  );
 }
--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const Dishes = () => {
   return (
-    <section className="container mx-auto py-16 px-8" id="dishes">
+    <section className="container mx-auto py-16 px-8 border-t-slate-500" id="dishes">
       <h2 className="mb-8 text-center text-3xl font-bold tracking-tighter lg:text-4xl">
         Our Dishes
       </h2>
